8d4a38653eac9d54bb6d68a1bd622681877115f4
walorey <facu-a77aque@hotmail.com.ar>
2018-12-25 17:08:19 -0300

    preparando barrita de administrador

--- resources/views/admin/template/partials/nav-admin.blade.php
+++ resources/views/admin/template/partials/nav-admin.blade.php
@@ -1,42 +1,35 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#"><img src="{{ asset('img/admin/reddema.png')}}"></a>
-    </div>
+
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
+      <a class="navbar-left" href="#"><img src="{{ asset('img/admin/reddema.png')}}"></a>
+
       <form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Buscar como administrador">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
+
+    <div class="btn-group navbar-left" role="group" aria-label="...">
+
+      <button type="button" class="btn btn-default">
+        <span class="glyphicon glyphicon-bell" aria-hidden="true"></span
+      </button>
+      <button type="button" class="btn btn-default">
+        <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+      </button>
+      
+    </div>
+
+    <div class="notificaciones navbar-left">
+       <span class="glyphicon glyphicon-eye-open ojo-admin" aria-hidden="true"></span>
+    </div>
+
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuario <span class="caret"></span></a>
           <ul class="dropdown-menu">
