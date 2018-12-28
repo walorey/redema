3e522bede84ef09df5e49e2a70066c9af992d122
walorey <facu-a77aque@hotmail.com.ar>
2018-12-28 02:15:08 -0300

    footer fijado abajo

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -18,37 +18,14 @@
 		@yield('content')
 	</div>
 
+
+	<footer class="footer-admin">
+		© 2018, Reddema, Todos los derechos reservados
+	</footer>
+
 	<script src="{{ asset('plugins/jquery/js/jquery-3.3.1.js') }}"></script>
 	<script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
 	
-	<div class="footer-bottom">
-
-	<div class="container">
-
-		<div class="row">
-
-			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-
-				<div class="copyright">
-
-					© 2018, Reddema, Todos los derechos reservados
-
-				</div>
-
-			</div>
-
-			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-
-				<div class="design">
-
-					 <!-- <a href="#">Franchisee </a> |  <a target="_blank" href="http://www.webenlance.com">Web Design & Development by Webenlance</a> -->
-					 Reddema | Red de mascotas
-
-				</div>
-
-			</div>
-
-		</div>
 
 </body>
 </html>
